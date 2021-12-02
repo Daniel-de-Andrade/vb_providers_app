8948435bbc46c7d5c8cdf3f1ea93a10976668ea2
Daniel Azevedo de Andrade <danielazevedoandra@hotmail.com>
2021-12-02 12:37:01 -0300

    criando nova branch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Vamos recomeçar o App VB Providers</h1>
+      <h1>Vamos recomeçar o App VB Providers!!</h1>
     </div>
   );
 }
