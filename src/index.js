906bf61721b8bd97bc77bc4288c3504843aa25f8
Andre Oliveira <tecnologia@grupogatti.com.br>
2021-12-02 13:31:08 -0300

    Finalizando configuração de rotas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import vbRoutes from './routes/index'
+import VbRoutes from './routes/index'
 
 ReactDOM.render(
   <React.StrictMode>
-    <vbRoutes />
+    <VbRoutes />
   </React.StrictMode>,
   document.getElementById("root")
 );
