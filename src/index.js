558fae3dfb9c3128fde56de510694daf812f822b
Andre Oliveira <tecnologia@grupogatti.com.br>
2021-12-02 14:10:16 -0300

    Adicionando ndo pasta assets

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import MainMenu from './components/mainMenu'
 import VbRoutes from './routes/index'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <VbRoutes />
+    <MainMenu />
   </React.StrictMode>,
   document.getElementById("root")
 );
